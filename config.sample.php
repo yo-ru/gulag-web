e617556bff0b2004e92e3aefaeb10bc27b4d9085
Nightsh0t <32829880+Nightsh0t@users.noreply.github.com>
2020-11-06 10:25:23 -0600

    Update config sample

--- config.sample.php
+++ config.sample.php
@@ -6,7 +6,21 @@ return (object) array(
   "username" => "cmyui",
   "password" => "changeme",
 
-  // Name of gulag instance
-  "instanceName" => "osu!gulag"
+  // Name of Gulag Instance
+  "instanceName" => "gulag-web",
+
+  // Disallowed Names
+  "disallowedNames" => array(
+    "cookiezi",
+    "rrtyui",
+    "hvick225",
+    "qsc20010"
+  ),
+
+  // Disallowed Passwords
+  "disallowedPasswords" => array(
+    "password",
+    "minilamp"
+  )
 );
 ?>
