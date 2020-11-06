950ae811c24d9f13d7517957ac7242a2ce94a623
Nightsh0t <32829880+Nightsh0t@users.noreply.github.com>
2020-11-06 10:14:25 -0600

    Metadata and Branding

--- assets/content/header.php
+++ assets/content/header.php
@@ -5,7 +5,7 @@ if (isset($_POST["logout"])) {
 }
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/">gulag</a>
+  <a class="navbar-brand" href="/"><?php echo $config->instanceName ?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
