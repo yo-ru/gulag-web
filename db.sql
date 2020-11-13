1b4cd94a7645715bacc658902d0cd4546b7c175a
yo-ru <32829880+Yo-ru@users.noreply.github.com>
2020-11-13 13:24:10 -0600

    Start our adventure to a py rewrite!

--- db.sql
+++
@@ -1,9 +0,0 @@
-# Remove pre-existing tables.
-drop table if exists user_sessions;
-
-create table user_sessions
-(
-  session_id varchar(255) not null primary key,
-  user_id int(11) not null,
-  login_time timestamp not null
-)
