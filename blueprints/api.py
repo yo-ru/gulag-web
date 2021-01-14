3c58d9e18952032b160cd0f2bb549ddf16277c08
Varkaria <61514399+HakuraSukinoe@users.noreply.github.com>
2021-01-14 12:25:02 +0700

    make get_leaderboard query easy to read it

--- blueprints/api.py
+++ blueprints/api.py
@@ -24,22 +24,20 @@ async def get_leaderboard():
 
     if country:
         res = await glob.db.fetchall(
-            f"SELECT ROW_NUMBER() OVER () AS id, users.id AS userid, users.name AS username, users.country, tscore_{mods}_{mode} AS tscore, rscore_{mods}_{mode} AS rscore, pp_{mods}_{mode} AS pp, "
-            f"plays_{mods}_{mode} AS plays, playtime_{mods}_{mode} AS playtime, acc_{mods}_{mode} AS acc, maxcombo_{mods}_{mode} AS maxcombo "
-            f"FROM stats "
-            f"JOIN users "
-            f"ON stats.id = users.id "
+            f"SELECT ROW_NUMBER() OVER () AS id, users.id AS userid, users.name AS username, users.country, tscore_{mods}_{mode} AS tscore, "
+            f"rscore_{mods}_{mode} AS rscore, pp_{mods}_{mode} AS pp, plays_{mods}_{mode} AS plays, playtime_{mods}_{mode} AS playtime, "
+            f"acc_{mods}_{mode} AS acc, maxcombo_{mods}_{mode} AS maxcombo FROM stats "
+            f"JOIN users ON stats.id = users.id "
             f"WHERE pp_{mods}_{mode} > 1 AND users.country = '{country}' "
             f"ORDER BY stats.{sort}_{mods}_{mode} DESC"
         )
     else:
         res = await glob.db.fetchall(
-            f"SELECT ROW_NUMBER() OVER () AS id, users.id AS userid, users.name AS username, users.country, tscore_{mods}_{mode} AS tscore, rscore_{mods}_{mode} AS rscore, pp_{mods}_{mode} AS pp, "
-            f"plays_{mods}_{mode} AS plays, playtime_{mods}_{mode} AS playtime, acc_{mods}_{mode} AS acc, maxcombo_{mods}_{mode} AS maxcombo "
-            f"FROM stats "
-            f"JOIN users "
-            f"ON stats.id = users.id "
-            f"WHERE pp_{mods}_{mode} > 1 "
+            f"SELECT ROW_NUMBER() OVER () AS id, users.id AS userid, users.name AS username, users.country, tscore_{mods}_{mode} AS tscore, "
+            f"rscore_{mods}_{mode} AS rscore, pp_{mods}_{mode} AS pp, plays_{mods}_{mode} AS plays, playtime_{mods}_{mode} AS playtime, "
+            f"acc_{mods}_{mode} AS acc, maxcombo_{mods}_{mode} AS maxcombo FROM stats "
+            f"JOIN users ON stats.id = users.id "
+            f"WHERE pp_{mods}_{mode} > 1"
             f"ORDER BY stats.{sort}_{mods}_{mode} DESC"
         )
 
