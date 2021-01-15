4b1ea1cd991dba1a8cf15fc80d53f1297b9e1df5
Nightsh0t <32829880+Nightsh0t@users.noreply.github.com>
2021-01-15 11:30:12 -0600

    template the rest of register for varkaria

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -10,15 +10,16 @@ __all__ = ()
 
 frontend = Blueprint('frontend', __name__)
 
-@frontend.route('/home')
+""" home """
+@frontend.route('/home') # GET
 @frontend.route('/')
 async def home():
     return await render_template('home.html')
 
+""" login """
 @frontend.route('/login') # GET
 async def login():
     return await render_template('login.html')
-
 @frontend.route('/login', methods=['POST']) # POST
 async def login_post():
     # get form data (username, password)
@@ -34,7 +35,7 @@ async def login_post():
     )
 
     # the second part of this if statement exists because if we try to login with Aika
-    # and compare our password inputed against the database it will fail because the 
+    # and compare our password input against the database it will fail because the 
     # hash saved in the database is invalid.
     if not user_info or user_info['id'] == 1:
         return b'login failed. account does not exist.'
@@ -48,17 +49,33 @@ async def login_post():
     # TODO: sessions and redirect
     if pw_bcrypt in bcrypt_cache:
         if pw_md5 != bcrypt_cache[pw_bcrypt]: # ~0.1ms
-            return 'login failed. password is incorrect.'
+            return b'login failed. password is incorrect.'
     else: # ~200ms
         if not bcrypt.checkpw(pw_md5, pw_bcrypt):
-            return 'login failed. password is incorrect.'
+            return b'login failed. password is incorrect.'
             
         # login success. cache password for next login
         bcrypt_cache[pw_bcrypt] = pw_md5
 
     # login successful
-    return 'login successful.'
+    return b'login successful.'
 
-@frontend.route("/register")
+
+""" register """
+# landing page
+@frontend.route('/register') # GET
 async def register():
-    return await render_template("register.html")
+    return await render_template('register.html')
+
+# in-game
+@frontend.route('/register/in-game') # GET
+async def register_in_game():
+    return b'not finished yet.'
+
+# online
+async def register_online(): # GET
+    return b'not finished yet.'
+@frontend.route('/register/online', methods=['POST']) # POST
+async def register_online_post():
+    return b'not finished yet.'
+    
