23432b58bb9ff1bf46b1a5f773934a72ca7813bf
Varkaria <61514399+HakuraSukinoe@users.noreply.github.com>
2021-01-16 22:40:06 +0700

    adding some stuff in navbar and change version of bulma-hsl

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,9 @@ $(window).scroll(() => {
         header.classList.remove("minimized");
     }
 });
+
+//toggle navbar for mobile
+function togglenavbar() {
+    document.getElementById('navbar').classList.toggle("is-active");
+    document.getElementById('navbar-burger').classList.toggle("is-active");
+  }
