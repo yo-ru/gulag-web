9d56c8ac5a9da4c0953fd15bfd11f34d1000f4f5
James <51536154+tsunyoku@users.noreply.github.com>
2021-03-18 20:35:23 +0000

    Update frontend.py

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -38,9 +38,12 @@ async def home():
     vn = await glob.db.fetch('SELECT pp, users.name FROM scores_vn LEFT JOIN users ON scores_vn.userid = users.id LEFT JOIN maps ON scores_vn.map_md5 = maps.md5 WHERE users.priv & 1 AND users.frozen = 0 AND maps.status = 2 ORDER BY pp DESC LIMIT 1')
     rx = await glob.db.fetch('SELECT pp, users.name FROM scores_rx LEFT JOIN users ON scores_rx.userid = users.id LEFT JOIN maps ON scores_rx.map_md5 = maps.md5 WHERE users.priv & 1 AND users.frozen = 0 AND maps.status = 2 ORDER BY pp DESC LIMIT 1')
     ap = await glob.db.fetch('SELECT pp, users.name FROM scores_ap LEFT JOIN users ON scores_ap.userid = users.id LEFT JOIN maps ON scores_ap.map_md5 = maps.md5 WHERE users.priv & 1 AND users.frozen = 0 AND maps.status = 2 ORDER BY pp DESC LIMIT 1')
-    return await render_template('home.html', vnpp=round(vn['pp']), vnuser=vn['name'], rxpp=round(rx['pp']), rxuser=rx['name'], appp=round(ap['pp']), apuser=ap['name'])
+    try:
+        return await render_template('home.html', vnpp=round(vn['pp']), vnuser=vn['name'], rxpp=round(rx['pp']), rxuser=rx['name'], appp=round(ap['pp']), apuser=ap['name'])
+    except:
+        return await render_template('home.html', vnpp=0, vnuser="None", rxpp=0, rxuser="None", appp=round0, apuser="None")
 
-""" settings """
+      """ settings """
 @frontend.route('/settings') # GET
 async def settings():
     # if not authenticated; render login
